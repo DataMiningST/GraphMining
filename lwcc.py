564cfc062752ba169d107d2654fab3a68abb2a69
Sebastian Schmidt <isibboi@gmail.com>
2016-12-10 11:21:59 +0200

    Updated LWCC to use snappies native algorithm.

--- lwcc.py
+++ lwcc.py
@@ -9,38 +9,22 @@ from UnionFind import *
 
 def computeLWCCInplace(graph):
     print('Computing largest weakly connected component...')
-    uf = UnionFind()
-    marked = set()
-
-    for edge in graph.Edges():
-        (source, destination) = edge.GetId()
-        uf.union(source, destination)
-
-    # Find weakly connected component with maximum size
-    ccSizes = {}
-    maxid = None
-    maxn = 0
-
+    components = TCnComV()
+    GetWccs(graph, components)
+    
+    lwcc = TCnComV()
+    
+    # Search largest WCC
+    for wcc in components:
+        if lwcc.Len() < wcc.Len():
+            lwcc = wcc
+            
+    # Convert WCC to node id set
+    lwcc = frozenset(lwcc)
+    
+    # Delete nodes not belonging to LWCC
     for node in graph.Nodes():
-        nid = uf[node.GetId()]
-
-        if nid in ccSizes:
-            ccSizes[nid] += 1
-
-            if ccSizes[nid] > maxn:
-                maxid = nid
-                maxn = ccSizes[nid]
-        else:
-            ccSizes[nid] = 1
-
-            if maxid == None:
-                maxid = nid
-                maxn = 1
-
-    for node in graph.Nodes():
-        nid = uf[node.GetId()]
-
-        if nid != maxid:
+        if not node.GetId() in lwcc:
             graph.DelNode(node.GetId())
 
     graph.Defrag()
