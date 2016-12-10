b7369daae10b5fc4d047efe535be6258bbf958ce
Sebastian Schmidt <isibboi@gmail.com>
2016-12-10 16:42:52 +0200

    Implemented all the statistics for the fm scheme

--- fmStatistics.py
+++ fmStatistics.py
@@ -29,6 +29,8 @@ for i in xrange(0, len(currentCounters)):
     currentCounters[i] = FMCounter(ncounters, counterWidth)
     currentCounters[i].initialize()
 
+# ANC-0 algorithm
+
 distances = [0] * (maxDistance + 1)
 
 for distance in xrange(1, maxDistance + 1):
@@ -40,10 +42,59 @@ for distance in xrange(1, maxDistance + 1):
     
     for counter in currentCounters:
         distances[distance] += counter.evaluate(True)
-        
-nodePairsWithDistance = [0] * maxDistance
+
+# Calculating distance histogram
+
+distanceHistogram = [0] * maxDistance
 for distance in xrange(1, maxDistance):
-    nodePairsWithDistance[distance] = (distances[distance + 1] - distances[distance])
+    distanceHistogram[distance] = (distances[distance + 1] - distances[distance])
+histogramSum = distances[maxDistance]
 
 print("Node pairs found: " + str(distances[maxDistance]) + "/" + str(graph.GetNodes()**2))
-print(nodePairsWithDistance)
+print(distanceHistogram)
+
+# Calculate median distance from histogram
+
+medianIndex = histogramSum // 2
+histogramIndex = 0
+
+for i, bucket in enumerate(distanceHistogram):
+    histogramIndex += bucket
+    
+    if histogramIndex >= medianIndex:
+        median = i
+        break
+
+print("Median distance: " + str(median))
+
+# Calculate mean distance from histogram
+
+mean = 0.0
+for i in xrange(len(distanceHistogram)):
+    mean += i * distanceHistogram[i]
+
+mean /= histogramSum
+print("Mean distance: " + str(mean))
+
+# Diameter (= max of all distances)
+
+for i, bucket in reversed(list(enumerate(distanceHistogram))):
+    if bucket != 0:
+        diameter = i
+        break
+
+print("Diameter: " + str(diameter))
+
+# Effective diameter
+
+edIndex = int(histogramSum * 0.9)
+histogramIndex = 0
+
+for i, bucket in enumerate(distanceHistogram):
+    histogramIndex += bucket
+    
+    if histogramIndex >= edIndex:
+        effectiveDiameter = i
+        break
+
+print("Effective diameter: " + str(effectiveDiameter))
