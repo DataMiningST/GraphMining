e534f518ff2b0711d87cf6e4e66e1c0fa0e4b438
Sebastian Schmidt <isibboi@gmail.com>
2016-12-23 12:40:20 +0100

    Improved performance of accurateStatistics.py

--- accurateStatistics.py
+++ accurateStatistics.py
@@ -8,21 +8,25 @@ from printStatistics import *
 from joblib import Parallel, delayed
 import multiprocessing
 
+import time
 
-def process_node(node):
+
+def process_nodes(nodes):
     global graph
     global isDirected
 
-    output = TIntPrV()
-    GetNodesAtHops(graph, node, output, isDirected)
+    out_list = [0]
 
-    out_list = [0] * output.Len()
+    for node in nodes:
+        output = TIntPrV()
+        GetNodesAtHops(graph, node, output, isDirected)
 
-    for pair in output:
-        if pair.GetVal1() >= len(out_list):
-            out_list += [0] * (pair.GetVal1() - len(out_list) + 1)
 
-        out_list[pair.GetVal1()] += pair.GetVal2()
+        for pair in output:
+            if pair.GetVal1() >= len(out_list):
+                out_list += [0] * (pair.GetVal1() - len(out_list) + 1)
+
+            out_list[pair.GetVal1()] += pair.GetVal2()
 
     return out_list
 
@@ -39,6 +43,8 @@ graph = TNGraph.Load(inStream)
 
 # Calculate distance histogram by making a full BFS for every node
 
+start_time = time.time()
+
 num_cores = multiprocessing.cpu_count()
 pool = multiprocessing.pool.ThreadPool(processes=num_cores)
 
@@ -46,7 +52,11 @@ inputs = []
 for node in graph.Nodes():
     inputs += [node.GetId()]
 
-results = pool.map(process_node, inputs)
+sliced_inputs = [None] * num_cores
+for i in xrange(num_cores):
+    sliced_inputs[i] = inputs[i * len(inputs) // num_cores:(i + 1) * len(inputs) // num_cores]
+
+results = pool.map(process_nodes, sliced_inputs)
 
 distanceHistogram = []
 for hist in results:
@@ -56,8 +66,11 @@ for hist in results:
     for i in xrange(len(hist)):
         distanceHistogram[i] += hist[i]
 
+duration = time.time() - start_time
+
 print(distanceHistogram)
 
+print("Duration: " + str(duration) + "s")
 printStatistics(distanceHistogram)
 
 
