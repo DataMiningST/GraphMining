2922aac32114b6b9fc735cae48dbb39dfbca07c0
Sebastian Schmidt <isibboi@gmail.com>
2016-12-23 12:41:43 +0100

    Parallelized preprocessing

--- preprocess.sh
+++ preprocess.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+mkdir -p ./results
+
 for file in ./data/*.txt; do
-	./lwcc.py "$file"
-	./lscc.py "$file"
+	basename=$(basename $file)
+	./lwcc.py "$file" > "/tmp/$basename-lwcc" &
+	./lscc.py "$file" > "/tmp/$basename-lscc" &
+done
+
+wait
+
+OUT="results/preprocessing.txt"
+
+for file in ./data/*.txt; do
+	basename=$(basename $file)
+	echo "$file" >> $OUT
+	echo "LWCC:" >> $OUT
+	cat "/tmp/$basename-lwcc" >> $OUT
+	echo "LSCC:" >> $OUT
+	cat "/tmp/$basename-lscc" >> $OUT
 done
