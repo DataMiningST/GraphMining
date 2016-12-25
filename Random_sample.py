d1dab426c9738c0f5bafb5d4b04bb0ea1ec322a3
Sebastian Schmidt <isibboi@gmail.com>
2016-12-25 11:57:57 +0100

    Sample size is set via program argument

--- Random_sample.py
+++ Random_sample.py
@@ -8,25 +8,20 @@ import numpy as np
 from snap import *
 
 
-if len(sys.argv) < 2:
-    error('No filename given')
+if len(sys.argv) < 3:
+    error('Please give the file name and the accuracy parameter')
 
 filename = sys.argv[1]
-
-samples = 100   #number of random samples for testing
+samples = int(sys.argv[2]) # Number of samples for estimation
 
 FIn = snap.TFIn(filename)
 Graph = snap.TNGraph.Load(FIn)
-directedOrNot = True #Whether the edges are considered directed or undirected, initialization
 
 #Directed graph if strongly connected, else undirected
-if ("lscc" in str(filename)): directedOrNot = True
-else: directedOrNot = False
+directedOrNot = "lscc" in str(filename)
 
 #calculate distances between sampled pairs
 distances = []
-a = 0
-b = 0
 
 for x in range(0, samples):
     a = Graph.GetRndNId()
@@ -43,7 +38,7 @@ diameter = np.max(distances)
 eff_diameter = distances[(samples)*9/10]
 
 print("Random pair sampling (" + str(samples) + " samples):")
-print("Approximate median distance: "+ str(median_distance))
-print("Approximate mean distance: "+ str(mean_distance))
-print("Approximate diameter: "+ str(diameter))
-print("Approximate effective diameter: "+ str(eff_diameter))
+print("Approximate median distance: " + str(median_distance))
+print("Approximate mean distance: " + str(mean_distance))
+print("Approximate diameter: " + str(diameter))
+print("Approximate effective diameter: " + str(eff_diameter))
