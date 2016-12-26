73d899b82112bbd7dccfd564c39c4cacaee486fb
Sebastian Schmidt <isibboi@gmail.com>
2016-12-26 11:05:07 +0100

    Added script to execute question2.1

---
+++ question2.1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+COMMAND=$1
+RESULTS_DIR=./results
+
+for file in ./data/*.txt; do
+	filename=$(basename $file)
+	./Random_sample.py "$file-lwcc.graph" 65536 > "$RESULTS_DIR/$filename-RS-undirected.txt" &
+	./Random_sample.py "$file-lscc.graph" 65536 > "$RESULTS_DIR/$filename-RS-directed.txt" &
+
+	./Random_sources_BFS.py "$file-lwcc.graph" 1024 > "$RESULTS_DIR/$filename-RBFS-undirected.txt" &
+	./Random_sources_BFS.py "$file-lscc.graph" 1024 > "$RESULTS_DIR/$filename-RBFS-directed.txt" &
+
+	./fms "$file-lwcc.graph" 64 > "$RESULTS_DIR/$filename-FM-undirected.txt" &
+	./fms "$file-lscc.graph" 64 > "$RESULTS_DIR/$filename-FM-directed.txt" &
+
+	wait
+done
