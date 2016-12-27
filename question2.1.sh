7305cbf2fdb3bd09271a9fd16ccb1346bd9bc2a6
Sebastian Schmidt <isibboi@gmail.com>
2016-12-27 11:48:26 +0100

    Reduced accuracy parameter for Q2.1

--- question2.1.sh
+++ question2.1.sh
@@ -5,8 +5,8 @@ RESULTS_DIR=./results
 
 for file in ./data/*.txt; do
 	filename=$(basename $file)
-	./Random_sample.py "$file-lwcc.graph" 65536 > "$RESULTS_DIR/$filename-RS-undirected.txt" &
-	./Random_sample.py "$file-lscc.graph" 65536 > "$RESULTS_DIR/$filename-RS-directed.txt" &
+	./Random_sample.py "$file-lwcc.graph" 16384 > "$RESULTS_DIR/$filename-RS-undirected.txt" &
+	./Random_sample.py "$file-lscc.graph" 16384 > "$RESULTS_DIR/$filename-RS-directed.txt" &
 
 	./Random_sources_BFS.py "$file-lwcc.graph" 1024 > "$RESULTS_DIR/$filename-RBFS-undirected.txt" &
 	./Random_sources_BFS.py "$file-lscc.graph" 1024 > "$RESULTS_DIR/$filename-RBFS-directed.txt" &
