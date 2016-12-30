017794d3e2ee77ccc38a39418067fec6cbfdee8c
Sebastian Schmidt <isibboi@gmail.com>
2016-12-30 23:23:18 +0100

    Added more FM tests

--- question2.2.sh
+++ question2.2.sh
@@ -3,7 +3,7 @@
 DATA=./data/gplus_combined.txt
 RS_VALUES="1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768"
 RBFS_VALUES="1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768"
-FM_VALUES="1 2 4 8 16 32 64 128 256 512 1024"
+FM_VALUES="1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192"
 
 echo "" > results/$(basename $DATA)-RS-undirected-multi.txt
 echo "" > results/$(basename $DATA)-RS-directed-multi.txt
